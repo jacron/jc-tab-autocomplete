d9a375a683127f50517f7146f6c095bb06f00eb3
jhsm croonen <info@jcroonen.nl>
2015-06-21 10:40:42 +0200

    ready and sample PHP added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # jc-tab-autocomplete
+
+jcTabAutocomplete is a directive for autocompleting a POSIX path (like on a Mac) on pressing the TAB
+##usage-example
+ 
+    <div jc-tab-autocomplete ng-model="myInput"></div>
+    
+##explanation
+In a terminal, you are used to tab autocompletion. Using this directive you get the same behaviour in an input tag.
+
+##dependencies
+The directive won't work unless you supply a back-end service. An example of a PHP implementation of such a service is included. See sample API.php.
+
+##installation
+bower install jc-tab-autocomplete
---
+++ sample API.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: orion
+ * Date: 21-06-15
+ */
+function getAllFiles($path) {
+    $files = array();
+    try {
+        foreach (new DirectoryIterator($path) as $item) {
+            if (substr($item, 0, 1) !== '.') {
+                $type = $item->isDir() ? 'dir' : 'file';
+                $files[] = array(
+                    'name' => $item->getFilename(),
+                    'type' => $type,
+                );
+            }
+        }
+        usort($files, function($a, $b) {
+            $ua = strtolower($a['name']);
+            $ub = strtolower($b['name']);
+            if ($ua === $ub) {
+                return 0;
+            }
+            return $ua > $ub ? 1 : -1;
+        });
+        return $files;
+
+    } catch (Exception $exc) {
+        return 'error in getAllFiles()';
+    }
+    return null;
+}
